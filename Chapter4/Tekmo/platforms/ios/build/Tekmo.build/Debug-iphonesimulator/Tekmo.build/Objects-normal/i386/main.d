1fcdcede33d2142f66c64c31dd448f9c4dc7b592
NickBranstein <nick@brosteins.com>
2016-06-16 23:15:16 -0400

    Added home-page.xml

---
+++ Chapter4/Tekmo/platforms/ios/build/Tekmo.build/Debug-iphonesimulator/Tekmo.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/main.m \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Nick/Git/Repos/NativeScriptInAction/Chapter4/Tekmo/platforms/ios/internal/TNSDebugging.h
