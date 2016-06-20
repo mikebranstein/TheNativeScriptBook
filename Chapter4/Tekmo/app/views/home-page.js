ffa4e87b0b930321e8051f487137179ac9c21768
NickBranstein <nick@brosteins.com>
2016-06-19 21:47:44 -0400

    added events

--- Chapter4/Tekmo/app/views/home-page.js
+++ Chapter4/Tekmo/app/views/home-page.js
@@ -1,7 +1,13 @@
 var frames = require("ui/frame");
+var navigationEntry = {
+    moduleName: "views/about-page",
+    transition: {
+        name: "slideBottom"
+    }
+};
 
 function onTap() {
-    frames.topmost().navigate("views/about-page");
+    frames.topmost().navigate(navigationEntry);
 }
 
 exports.onTap = onTap;
