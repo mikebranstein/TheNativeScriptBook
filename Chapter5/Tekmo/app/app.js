f7381dfeb2b194afa409fe0c8cc5bfc22b759715
Mike Branstein <mike.branstein@gmail.com>
2016-06-21 16:49:26 -0400

    Add Tekmo home page to chapter 5

---
+++ Chapter5/Tekmo/app/app.js
@@ -0,0 +1,11 @@
+var application = require("application");
+application.cssFile = "./app.css";
+application.start({ moduleName: "views/home-page" });
+
+application.on(application.launchEvent, function (args) {
+    if (args.android) {
+        
+    } else if (args.ios !== undefined) {
+       
+    }
+});
