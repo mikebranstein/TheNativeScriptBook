72e975b5c9027966598cedfacefc57d1dab721f3
Mike Branstein <mike.branstein@gmail.com>
2016-07-10 20:48:53 -0400

    Move home page verbiage to about us

--- Chapter5/Tekmo/app/app.js
+++ Chapter5/Tekmo/app/app.js
@@ -1,6 +1,6 @@
 var application = require("application");
 application.cssFile = "./app.css";
-application.start({ moduleName: "views/home-page/home-page" });
+application.start({ moduleName: "views/home/home" });
 
 application.on(application.launchEvent, function (args) {
     if (args.android) {
