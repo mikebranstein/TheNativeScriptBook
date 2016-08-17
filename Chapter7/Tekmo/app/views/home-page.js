b3a5a87b6a9715394bd3266ef51febde89bbe372
Mike Branstein <mike.branstein@gmail.com>
2016-08-16 21:23:22 -0400

    initial commit

---
+++ Chapter7/Tekmo/app/views/home-page.js
@@ -0,0 +1,35 @@
+var frames = require("ui/frame");
+
+function onTap_About() {
+    var navigationEntry = {
+        moduleName: "views/about-page",
+        transition: {
+            name: "slideBottom"
+        }
+    };
+    frames.topmost().navigate(navigationEntry);
+}
+exports.onTap_About = onTap_About;
+
+function onTap_ContactUs() {
+    var navigationEntry = {
+        moduleName: "views/contact-us-page",
+        transition: {
+            name: "slideBottom"
+        }
+    };
+    frames.topmost().navigate(navigationEntry);
+}
+exports.onTap_ContactUs = onTap_ContactUs;
+
+function onTap_Products() {
+    var navigationEntry = {
+        moduleName: "views/products-page",
+        clearHistory: true,
+        transition: {
+            name: "slideBottom"
+        }
+    };
+    frames.topmost().navigate(navigationEntry);
+}
+exports.onTap_Products = onTap_Products;
