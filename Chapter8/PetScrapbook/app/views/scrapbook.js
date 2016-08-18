c00583b53bea8c34fa9cc5134e170d86f426c2d6
NickBranstein <nick@brosteins.com>
2016-08-17 21:49:28 -0400

    updated to include databinding to a label

--- Chapter8/PetScrapbook/app/views/scrapbook.js
+++ Chapter8/PetScrapbook/app/views/scrapbook.js
@@ -1,20 +1,29 @@
 var observable = require("data/observable");
+var observableArray = require("data/observable-array");
 
 exports.onLoaded = function(args) {
     var page = args.object;
-    var scrapbook = new observable.Observable({
+    var emptyScrapbookPage = new observable.Observable({
         genders: ["Female", "Male", "Other"]
     });
-    
+    var filledScrapbookPage = new observable.Observable({
+        genders: ["Female", "Male", "Other"],
+        title: "Riven's Page",
+        gender: 0
+    });
+    var scrapbook = new observable.Observable({
+        pages: new observableArray.ObservableArray(emptyScrapbookPage, filledScrapbookPage)
+    });
+
     page.bindingContext = scrapbook;
 };
 
 exports.onTap = function(args) {
-    var page = args.object;
-    var scrapbook = page.bindingContext;
-    var date = new Date(scrapbook.year, scrapbook.month, scrapbook.day);
+    // var page = args.object;
+    // var scrapbook = page.bindingContext;
+    // var date = new Date(scrapbook.year, scrapbook.month, scrapbook.day);
     
-    console.log("You have made " + scrapbook.title);
-    console.log("Age: " + date.toLocaleDateString());
-    console.log("Gender selected:" + scrapbook.gender);
+    // console.log("You have made " + scrapbook.title);
+    // console.log("Age: " + date.toLocaleDateString());
+    // console.log("Gender selected:" + scrapbook.gender);
 }
--- Chapter8/observable-example/app/main-page.js
+++ Chapter8/observable-example/app/main-page.js
@@ -1,14 +1,27 @@
 var observableModule = require("data/observable");
-var pet = new observableModule.Observable();
+var view = require ("ui/core/view");
 
-pet.set("Name", "Riven");
-pet.set("Type", "Dog");
-pet.set("Age", 3);
+exports.onLoaded = function(args){
+    var page = args.object;
+    var pet = new observableModule.Observable();
+    var label = view.getViewById(page, "title");
 
-pet.on ("propertyChange", function(eventData){
-    var changedPet = eventData.object;
+    var bindingOptions = {
+        sourceProperty: "Name",
+        targetProperty: "text"
+    };
 
-    console.log("Your pet is a " + changedPet.Type + " named " + changedPet.Name + " and is " + changedPet.get("Age") + " years old.");
-});
+    label.bind(bindingOptions, pet);
 
-pet.set("Age", 4);
+    pet.set("Name", "Riven");
+    pet.set("Type", "Dog");
+    pet.set("Age", 3);
+
+    pet.on("propertyChange", function(eventData){
+        var changedPet = eventData.object;
+
+        console.log("Your pet is a " + changedPet.Type + " named " + changedPet.Name + " and is " + changedPet.get("Age") + " years old.");
+    });
+
+    pet.set("Age", 4);
+}
