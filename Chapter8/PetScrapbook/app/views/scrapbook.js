dcdbed61bbc736b5154be605ad433e37c38d5d11
NickBranstein <nick@brosteins.com>
2016-08-18 00:34:27 -0400

    finished the example of binding to an observalbe array and a listview Item template

--- Chapter8/PetScrapbook/app/views/scrapbook.js
+++ Chapter8/PetScrapbook/app/views/scrapbook.js
@@ -3,12 +3,22 @@ var observableArray = require("data/observable-array");
 
 exports.onLoaded = function(args) {
     var page = args.object;
+    var calcAge = function(year, month, day){
+            var date = new Date(year, month, day);
+            var now = Date.now();
+            var diff = Math.abs(now - date) / 1000 / 31536000;
+
+            return diff.toFixed(1);
+    }
+    var genders = ["Female", "Male", "Other"];
     var emptyScrapbookPage = new observable.Observable({
-        genders: ["Female", "Male", "Other"]
+        genders: genders,
+        calcAge: calcAge
     });
     var filledScrapbookPage = new observable.Observable({
-        genders: ["Female", "Male", "Other"],
+        genders: genders,
         title: "Riven's Page",
+        calcAge: calcAge,
         gender: 0
     });
     var scrapbook = new observable.Observable({
@@ -19,11 +29,11 @@ exports.onLoaded = function(args) {
 };
 
 exports.onTap = function(args) {
-    // var page = args.object;
-    // var scrapbook = page.bindingContext;
-    // var date = new Date(scrapbook.year, scrapbook.month, scrapbook.day);
+    var page = args.object;
+    var scrapbook = page.bindingContext;
+    var date = new Date(scrapbook.year, scrapbook.month, scrapbook.day);
     
-    // console.log("You have made " + scrapbook.title);
-    // console.log("Age: " + date.toLocaleDateString());
-    // console.log("Gender selected:" + scrapbook.gender);
+    console.log("You have made " + scrapbook.title);
+    console.log("Age: " + date.toLocaleDateString());
+    console.log("Gender selected:" + scrapbook.gender);
 }
