332385461bb7a70545d5e1048856f56d817d7d94
NickBranstein <nick@brosteins.com>
2016-08-18 19:56:05 -0400

    create basic file system example

---
+++ Chapter8/filesystem-example/app/main-page.js
@@ -0,0 +1,15 @@
+var fileSystemModule = require("file-system");
+
+exports.onLoaded = function(){
+    var fileName = "myFile.json";
+    var file = fileSystemModule.knownFolders.documents().getFile(fileName);
+    var data = {name: "Brosteins", type: "filesystemexample"};
+    var json = JSON.stringify(data);
+
+    file.writeText(json);
+
+    var theFile = file.readTextSync();
+
+    console.log("The 'file' is: " + json);
+    console.log("The 'theFile' is: " + theFile);
+};
