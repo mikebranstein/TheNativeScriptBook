818d4970dde0799d7e9f18f51e4c296a2b05cc2a
NickBranstein <nick@brosteins.com>
2016-08-18 22:15:07 -0400

    Added saving/loading to the file system

--- Chapter8/PetScrapbook/app/views/scrapbook.js
+++ Chapter8/PetScrapbook/app/views/scrapbook.js
@@ -1,29 +1,44 @@
 var observable = require("data/observable");
 var observableArray = require("data/observable-array");
+var fileSystem = require("file-system");
 
 exports.onLoaded = function(args) {
     var page = args.object;
-    var calcAge = function(year, month, day){
+    var file = fileSystem.knownFolders.documents().getFile("scrapbook.json").readTextSync();
+    var emptyScrapbookPage = new observable.Observable({
+        genders: ["Female", "Male", "Other"],
+        calcAge: function(year, month, day){
             var date = new Date(year, month, day);
             var now = Date.now();
             var diff = Math.abs(now - date) / 1000 / 31536000;
 
             return diff.toFixed(1);
-    }
-    var genders = ["Female", "Male", "Other"];
-    var emptyScrapbookPage = new observable.Observable({
-        genders: genders,
-        calcAge: calcAge
-    });
-    var filledScrapbookPage = new observable.Observable({
-        genders: genders,
-        title: "Riven's Page",
-        calcAge: calcAge,
-        gender: 0
+        }
     });
     var scrapbook = new observable.Observable({
-        pages: new observableArray.ObservableArray(emptyScrapbookPage, filledScrapbookPage)
+        pages: new observableArray.ObservableArray(emptyScrapbookPage)
     });
+
+    if(file.length !== 0){
+        var pages = JSON.parse(file);
+        pages.forEach(function(item){
+            scrapbook.pages.push(new observable.Observable({
+                genders: ["Female", "Male", "Other"],
+                calcAge: function(year, month, day){
+                    var date = new Date(year, month, day);
+                    var now = Date.now();
+                    var diff = Math.abs(now - date) / 1000 / 31536000;
+
+                    return diff.toFixed(1);
+                },
+                title: item.title,
+                gender: item.gender,
+                year: item.year,
+                month: item.month,
+                day: item.day
+            }));
+        });
+    }
 
     page.bindingContext = scrapbook;
 };
@@ -31,9 +46,20 @@ exports.onLoaded = function(args) {
 exports.onTap = function(args) {
     var page = args.object;
     var scrapbook = page.bindingContext;
-    var date = new Date(scrapbook.year, scrapbook.month, scrapbook.day);
+    var file = fileSystem.knownFolders.documents().getFile("scrapbook.json");
+    var pages = [];
     
-    console.log("You have made " + scrapbook.title);
-    console.log("Age: " + date.toLocaleDateString());
-    console.log("Gender selected:" + scrapbook.gender);
+    scrapbook.pages.forEach(function (item) {
+        pages.push({
+            gender: item.gender, 
+            year: item.year, 
+            month: item.month, 
+            day: item.day, 
+            title: item.title
+        });
+    });
+
+    var json = JSON.stringify(pages);
+
+    file.writeText(json);
 }
