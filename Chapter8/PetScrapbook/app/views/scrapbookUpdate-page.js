68a32eed400f72a294b50dd020ab60e91dbeea08
NickBranstein <nick@brosteins.com>
2016-10-02 16:03:10 -0400

    removed index

--- Chapter8/PetScrapbook/app/views/scrapbookUpdate-page.js
+++ Chapter8/PetScrapbook/app/views/scrapbookUpdate-page.js
@@ -1,11 +1,10 @@
 var observable = require("data/observable");
 var frame = require("ui/frame");
-var index;
 var scrapbook;
 
 exports.onLoaded = function(args) {
     var page = args.object;
-    index = page.navigationContext.index;
+    var index = page.navigationContext.index;
     scrapbook = page.navigationContext.model
 
     page.bindingContext = scrapbook.pages.getItem(index);
