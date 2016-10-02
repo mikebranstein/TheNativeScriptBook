c5bde8934561ffd09ea5466f1749c1e469236e39
NickBranstein <nick@brosteins.com>
2016-10-02 16:02:05 -0400

    updated naming

--- Chapter8/PetScrapbook/app/views/scrapbookUpdate-page.js
+++ Chapter8/PetScrapbook/app/views/scrapbookUpdate-page.js
@@ -1,21 +1,21 @@
 var observable = require("data/observable");
 var frame = require("ui/frame");
 var index;
-var model;
+var scrapbook;
 
 exports.onLoaded = function(args) {
     var page = args.object;
     index = page.navigationContext.index;
-    model = page.navigationContext.model
+    scrapbook = page.navigationContext.model
 
-    page.bindingContext = model.pages.getItem(index);
+    page.bindingContext = scrapbook.pages.getItem(index);
 };
 
 exports.onDoneTap = function(args) {    
     var page = args.object;
-    
+
     frame.topmost().navigate({ 
         moduleName: "views/scrapbook-page", 
-        context: { model: model }
+        context: { model: scrapbook }
     });
 };
