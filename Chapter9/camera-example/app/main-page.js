29e0091d9a35d2d7165c6301e34ba9ea34196d79
NickBranstein <nick@brosteins.com>
2016-10-11 15:32:31 -0400

    camera example

---
+++ Chapter9/camera-example/app/main-page.js
@@ -0,0 +1,20 @@
+var camera = require("camera");
+var imageModule = require("ui/image");
+
+var myImage;
+
+exports.onLoaded = function (args) {
+    var page = args.object;
+    myImage = page.getViewById("myImage");
+}
+
+exports.onTap = function () {
+    camera.takePicture().then(function(picture) {
+        console.log("Result is an image source instance");
+        
+        var image = new imageModule.Image();
+        image.imageSource = picture;
+
+        myImage.imageSource = picture;
+    });
+}
