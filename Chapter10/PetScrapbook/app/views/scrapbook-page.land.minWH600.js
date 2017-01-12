1e692ce6f58d42fe7da525c88d6a1a44bf294d7c
Mike Branstein <mike.branstein@gmail.com>
2017-01-11 21:37:05 -0500

    tablet view added

--- Chapter10/PetScrapbook/app/views/scrapbook-page.land.minWH600.js
+++ Chapter10/PetScrapbook/app/views/scrapbook-page.land.minWH600.js
@@ -4,11 +4,15 @@ var frame = require("ui/frame");
 var view = require("ui/core/view");
 var fileSystemService = require("~/data/fileSystemService");
 
+var camera = require("camera");
+var geolocation = require("nativescript-geolocation");
+
 var scrapbook;
+var page;
 
 function scrapbookPageModel(){
     var model = new observable.Observable({ 
-        title: "null", 
+        title: null, 
         birthDate: null, 
         gender: null, 
         image: null,
@@ -27,10 +31,17 @@ function scrapbookPageModel(){
 }
 
 exports.onLoaded = function(args) {
-    var page = args.object;
+    page = args.object;
+    
     var scrapbook = new observable.Observable({
         pages: new observableArray.ObservableArray(),
-        selectedPage: null
+        selectedPage: null,
+        calcAge: function(birthDate){
+            var now = Date.now();
+            var diff = Math.abs(now - birthDate) / 1000 / 31536000;
+
+            return diff.toFixed(1);
+        }
     });
 
     var pages = fileSystemService.fileSystemService.getPages();
@@ -52,6 +63,9 @@ exports.onLoaded = function(args) {
     }
 
     page.bindingContext = scrapbook;
+    var stackLayout = view.getViewById(page, "temp");
+    stackLayout.bindingContext = scrapbook.selectedPage;
+    console.log("done");
 };
 
 exports.onAddTap = function(args) {
@@ -61,13 +75,68 @@ exports.onAddTap = function(args) {
     scrapbook.pages.push(new scrapbookPageModel());
 };
 
-function onItemTap (args) {
-    var page = args.object;
+exports.onItemTap = function(args) {
     var scrapbook = page.bindingContext;
+
+    scrapbook.pages.forEach(function (item) {
+        item.set("isActive", false);
+    });
+
     scrapbook.set("selectedPage", scrapbook.pages.getItem(args.index));
+    scrapbook.selectedPage.set("isActive", true);
     console.log(JSON.stringify(scrapbook.selectedPage));
 }
 
-exports.test = function (args) {
-    console.log("testing...");
+exports.onBirthDateTap = function(args) {
+    var modalPageModule = "views/selectDate-page";
+    var context = { birthDate: page.bindingContext.birthDate };
+    var fullscreen = true;
+
+    page.showModal(modalPageModule, context, function closeCallback(birthDate) {
+        page.bindingContext.selectedPage.set("birthDate", birthDate);
+    }, fullscreen);
+};
+
+exports.onGenderTap = function(args) {
+    var modalPageModule = "views/selectGender-page";
+    var context = { gender: page.bindingContext.gender };
+    var fullscreen = true;
+    page.showModal(modalPageModule, context, function closeCallback(gender) {
+        page.bindingContext.selectedPage.set("gender", gender);
+    }, fullscreen);
+};
+
+exports.onDoneTap = function(args) {    
+    var scrapbookPage = page.bindingContext;
+    
+    fileSystemService.fileSystemService.savePage(scrapbookPage);
+
+    frame.topmost().navigate({ 
+        moduleName: "views/scrapbook-page",
+        clearHistory: true,
+        transition: {
+            name: "slideRight"
+        }
+    });
+};
+
+exports.onAddImageTap = function (args) {
+    var scrapbookPage = page.bindingContext;
+
+    if (!geolocation.isEnabled()) {
+        geolocation.enableLocationRequest();
+    }
+
+    camera
+        .takePicture({ width: 100, height: 100, keepAspectRatio: true })
+        .then(function (picture) {
+            scrapbookPage.set("image", picture);
+
+            geolocation
+                .getCurrentLocation()
+                .then(function (location) {
+                    scrapbookPage.set("lat", location.latitude);
+                    scrapbookPage.set("long", location.longitude);
+                });
+        });
 };
