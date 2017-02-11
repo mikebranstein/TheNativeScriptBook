f3f3be6691a2fc7580b2642e6d06c8e433b7efe5
Mike Branstein <mike.branstein@gmail.com>
2017-02-11 15:29:06 -0500

    initial commit for ch 13

---
+++ Chapter13/PetScrapbook/app/views/shared/scrapbook-list/scrapbook-list.js
@@ -0,0 +1,57 @@
+var GridLayout = require("ui/layouts/grid-layout").GridLayout;
+var Label = require("ui/label").Label;
+var ListView = require("ui/list-view").ListView;
+ 
+var ScrapbookList = (function (_super) {
+    global.__extends(ScrapbookList, _super);
+
+    Object.defineProperty(ScrapbookList.prototype, "items", {
+        get: function(){
+            return this._items;
+        },
+        set: function(value){
+            this._items = value;
+            this.bindData();
+        }
+    });
+
+    function ScrapbookList() {
+        _super.call(this);
+
+        this._items;
+        this._itemTap;
+
+        this.rows = "*";
+        this.columns = "*";
+
+        var listView = new ListView();
+        listView.cssClass = "list-group";
+        listView.itemTemplate = '<StackLayout orientation="horizontal" class="{{ isActive ? \'list-group-item active\' : \'list-group-item\' }}"><Image class="thumb img-circle" src="{{ image }}" /><Label class="list-group-item-text" style="width: 100%" textWrap="true" text="{{ title, (title === null || title === undefined ? \'New\' : title + \'\\\'s\') + \' Scrapbook Page\' }}" /></StackLayout>';
+        listView.on(ListView.itemTapEvent, function(args) {
+            onItemTap(this, args.index);
+        }.bind(listView)); 
+
+        this.addChild(listView);
+
+        this.bindData = function () {
+            var bindingOptions = {
+                sourceProperty: "$value",
+                targetProperty: "items",
+                twoWay: "true"
+            };
+            listView.bind(bindingOptions, this._items);
+        };
+
+        var onItemTap = function(args, index) {
+            this.notify({
+                eventName: "itemTap",
+                object: this,
+                index: index
+            });
+        }.bind(this);
+    }
+    return ScrapbookList;
+ 
+})(GridLayout);
+ 
+exports.ScrapbookList = ScrapbookList;
