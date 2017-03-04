6851a650d28d3d325b04afbe7fe042c1dd51ee1d
NickBranstein <nick@brosteins.com>
2017-03-03 21:33:31 -0500

    initial chapter 14/15 check in

---
+++ Chapter14/PetScrapbookAngular/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App"></ActionBar>
+    <!-- Your UI components go here -->
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
