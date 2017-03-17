f2c661ba4646f8eeabb4740cb16c5817e091e1f9
NickBranstein <nick@brosteins.com>
2017-03-17 16:14:00 -0400

    setup to navigate to detail

--- Chapter15/PetScrapbookAngular/app/views/list/list.component.ts
+++ Chapter15/PetScrapbookAngular/app/views/list/list.component.ts
@@ -1,23 +1,35 @@
 import { Component } from "@angular/core";
 import { Page } from "../../models/page";
+import { Router, NavigationExtras } from "@angular/router";
 
 @Component({
     selector: "list",
     templateUrl: "views/list/list.html"
 })
-    
+
 export class ListComponent {
     public pages: Array<Page>;
 
-    constructor() {
-
+    constructor(private router: Router) {
     }
 
     onAddTap(): void {
+        let navigationExtras: NavigationExtras = {
+          queryParams: {
+            //"continueGame": true
+          }
+        };
         
+        this.router.navigate(["detail"], navigationExtras);
     }
 
     onItemTap(): void {
+        let navigationExtras: NavigationExtras = {
+          queryParams: {
+            //"continueGame": true
+          }
+        };
         
+        this.router.navigate(["detail"], navigationExtras);
     }
 }
