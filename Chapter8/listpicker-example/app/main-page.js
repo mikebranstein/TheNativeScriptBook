e923a2ebc280f8ca387a4eea30e475d8ed77e33e
NickBranstein <nick@brosteins.com>
2017-05-29 10:15:30 -0400

    updated all examples to NS 3.0

--- Chapter8/listpicker-example/app/main-page.js
+++ Chapter8/listpicker-example/app/main-page.js
@@ -2,9 +2,9 @@ var observable = require("data/observable");
 
 exports.onLoaded = function(args){
     var page = args.object;
-    var listItems = new observable.Observable({
-        items: ["Item 1", "Item 2", "Item 3"]
-    });
+    var listItems = new observable.Observable();
+
+    listItems.items = ["Item 1", "Item 2", "Item 3"];
 
     page.bindingContext = listItems;
 }
